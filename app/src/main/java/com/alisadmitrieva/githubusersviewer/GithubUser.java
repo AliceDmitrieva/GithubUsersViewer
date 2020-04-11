dba60b3be7d4270e5205be38c1f6b036f65951a3
Alice <coolalisa@yandex.ru>
2020-04-11 23:23:00 +0300

    Made GithubUser model extending Parcelable.

--- app/src/main/java/com/alisadmitrieva/githubusersviewer/GithubUser.java
+++ app/src/main/java/com/alisadmitrieva/githubusersviewer/GithubUser.java
@@ -1,8 +1,11 @@
 package com.alisadmitrieva.githubusersviewer;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class GithubUser {
+public class GithubUser implements Parcelable {
 
     @SerializedName("id")
     private int id;
@@ -36,5 +39,37 @@ public class GithubUser {
     public void setAvatarUrl(String avatarUrl) {
         this.avatarUrl = avatarUrl;
     }
+
+
+    protected GithubUser(Parcel in) {
+        id = in.readInt();
+        login = in.readString();
+        avatarUrl = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(login);
+        dest.writeString(avatarUrl);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<GithubUser> CREATOR = new Parcelable.Creator<GithubUser>() {
+        @Override
+        public GithubUser createFromParcel(Parcel in) {
+            return new GithubUser(in);
+        }
+
+        @Override
+        public GithubUser[] newArray(int size) {
+            return new GithubUser[size];
+        }
+    };
 
 }
