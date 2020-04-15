23f1be2d6d3c0020d3400f28a5944d3a8a4be9f2
Alice <coolalisa@yandex.ru>
2020-04-15 21:17:24 +0300

    Replaced FragmentStateAdapter with RecyclerView.Adapter.

--- app/src/main/java/com/alisadmitrieva/githubusersviewer/githubusers/GithubUsersViewPagerAdapter.java
+++ app/src/main/java/com/alisadmitrieva/githubusersviewer/githubusers/GithubUsersViewPagerAdapter.java
@@ -1,34 +1,59 @@
 package com.alisadmitrieva.githubusersviewer.githubusers;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.alisadmitrieva.githubusersviewer.GithubUser;
+import com.alisadmitrieva.githubusersviewer.R;
 
 import java.util.List;
 
-public class GithubUsersViewPagerAdapter extends FragmentStateAdapter {
+public class GithubUsersViewPagerAdapter extends RecyclerView.Adapter<GithubUsersViewPagerAdapter.GithubUsersViewHolder> {
 
     private AppCompatActivity activity;
     private List<GithubUser> githubUsers;
 
     public GithubUsersViewPagerAdapter(AppCompatActivity activity, List<GithubUser> githubUsers) {
-        super(activity);
         this.activity = activity;
         this.githubUsers = githubUsers;
     }
 
     @NonNull
     @Override
-    public Fragment createFragment(int position) {
-        return GithubUsersPageFragment.newInstance(githubUsers);
+    public GithubUsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.github_users_page_fragment, parent, false);
+        return new GithubUsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GithubUsersViewHolder holder, int position) {
+        holder.bindGithubUsers();
     }
 
     @Override
     public int getItemCount() {
         return githubUsers.size();
+    }
+
+    class GithubUsersViewHolder extends RecyclerView.ViewHolder {
+
+        GridView gridView;
+
+        GithubUsersViewHolder(@NonNull View itemView) {
+            super(itemView);
+            gridView = itemView.findViewById(R.id.gridview);
+        }
+
+        void bindGithubUsers() {
+            GithubUsersAdapter githubUsersAdapter = new GithubUsersAdapter(activity, githubUsers);
+            gridView.setAdapter(githubUsersAdapter);
+        }
     }
 
 }
